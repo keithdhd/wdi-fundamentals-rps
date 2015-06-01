ad9a7493c3ff16e18f9673d87b6ae58a902f6cab
Keith Douglas <keithdouglas@Keiths-Air.lan>
2015-06-01 10:11:59 +0100

    getWinner function added

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -25,22 +25,47 @@ function getPlayerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `getInput()`.
-    return /* Your Expression */;
+    return move || getInput();
 }
 
 function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return /* Your Expression */;
+    return move || randomPlay();
 }
 
 function getWinner(playerMove,computerMove) {
-    var winner;
+    var winner = "tie";
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
     /* YOUR CODE HERE */
+    if(playerMove == "rock"){
+        if(computerMove == "scissors"){
+            winner = "player";
+        }
+        else if(computerMove == "paper"){
+            winner = "computer";
+        }
+        
+    }
+    else if(playerMove == "scissors"){
+        if(computerMove == "rock"){
+            winner = "computer";
+        }
+        else if(computerMove == "paper"){
+            winner = "player";
+        }
+    }
+    else if(playerMove == "paper"){
+        if(computerMove == "rock"){
+            winner = "player";
+        }
+        else if(computerMove == "scissors"){
+            winner = "computer";
+        }
+    }
     return winner;
 }
 
